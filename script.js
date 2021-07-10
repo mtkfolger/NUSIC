fe29d16a42e750e5179e65bb9ecbda2c0e0e33dd
harrisonarmstrong93@gmail.com <harrisonarmstrong93@gmail.com>
2021-07-10 12:15:01 -0400

    Added Countries to the geochart and chart now returns iso 3166-2 code to api call

--- script.js
+++ script.js
@@ -1,67 +1,101 @@
 google.charts.load('current', {
-    'packages':['geochart'],
-    // Note: you will need to get a mapsApiKey for your project.
-    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-    'mapsApiKey': 'AIzaSyAicUJMDGxg1fEo1GRcNo3fGZhtf25rQGw'
-  });
-  google.charts.setOnLoadCallback(drawRegionsMap);
+  'packages':['geochart'],
+  // Note: you will need to get a mapsApiKey for your project.
+  // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+  'mapsApiKey': `${config.mapsApiKey}`
+});
 
-  function drawRegionsMap() {
-    var data = google.visualization.arrayToDataTable([
-      ['Country'],
-      ['Germany'],
-      ['United States'],
-      ['Brazil'],
-      ['Canada'],
-      ['France'],
-      ['Russia']
-    ])
-    var options = {
-      backgroundColor: 'none'
+google.charts.setOnLoadCallback(drawRegionsMap);
+
+function drawRegionsMap() {
+  var data = google.visualization.arrayToDataTable([
+    ['Country'],
+    ['Germany'],
+    ['United States'],
+    ['Brazil'],
+    ['Canada'],
+    ['France'],
+    ['Russia'],
+    ['Peru'],
+    ['Japan'],
+    ['Ghana'],
+    ['South Korea'],
+    ['China'],
+    ['Greenland'],
+    ['Spaine'],
+    ['Egypt'],
+    ['Australia'],
+    ['New Zealand'],
+    ['Ivory Coast'],
+    ['South Afrifa'],
+    ['Madagascar'],
+    ['India']
+  ]);
+  var options = {
+    backgroundColor: 'none',
+    datalessRegionColor: 'black',
         
-    };
+  };
 
-    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+  var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
 
-    google.visualization.events.addListener(chart, 'select', chartSelectedHandler)
+  google.visualization.events.addListener(chart, 'select', chartSelectedHandler)
 
-    function chartSelectedHandler(event) {
-        console.log(chart.getSelection());
-        selection = chart.getSelection();
+  function chartSelectedHandler(event) {
+    selection = chart.getSelection();
+    country = data.getValue(selection[0].row, 0);
+    getCountryCode(country);
 
-        countryrow = selection[0].row;
-        console.log(countryrow);
-        console.log(data.getValue(countryrow, 0));
-
-    }
-
-    chart.draw(data, options);
   }
 
+  chart.draw(data, options);
+}
 
 
-
-function getcountryTopChart() {
-  var nusixCall = `https://api.musixmatch.com/ws/1.1/chart.tracks.get?apikey=${config.NusicApiKey}&chart_name=top&page=1&page_size=25&country=it&f_has_lyrics=1`
-
-  fetch(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?apikey=a796737fb3bafc9bde347d07ad7780c1&chart_name=top&page=1&page_size=25&country=it&f_has_lyrics=1`)
+function getcountryTopChart(countryCode) {
+  fetch(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?apikey=a796737fb3bafc9bde347d07ad7780c1&chart_name=top&page=1&page_size=25&country=${countryCode}&f_has_lyrics=1`)
   .then(response => response.json())
   .then(function(data) {
     var tracklist = data.message.body.track_list
     console.log(tracklist);
     console.log(tracklist[0].track);
    
-    var list = $('<ul>');
-    var li = $('<li>', {
-      text: `${tracklist[0].track.track_name}`
-    });
 
-    list.append(li);
-    main.append(list)
   })
 
 }
-getcountryTopChart();
+
+function getCountryCode(country) {
+  var codes = {
+    'Germany': 'DE',
+    'United States': 'US',
+    'Brazil': 'BR',
+    'Canada': 'CA',
+    'France': 'FR',
+    'Russia': 'RU',
+    'Peru': 'PE',
+    'Japan': 'JP',
+    'Ghana': 'GH',
+    'South Korea': 'KR' ,
+    'China': 'CN',
+    'Greenland': 'GL',
+    'Spain': 'ES' ,
+    'Egypt': 'EG',
+    'Australia': 'AU',
+    'New Zealand': 'NZ',
+    'Ivory Coast': 'CI',
+    'South Africa': 'ZA',
+    'Madagascar': 'MG',
+    'India': 'IN'
+
+  }
+  getcountryTopChart(codes[country]);
+}
+
+
+
+
+//getcountryTopChart();
 
 
 
