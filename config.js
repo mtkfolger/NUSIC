4d2f9986b34e50860f7b592701d982bcb1a2385e
harrisonarmstrong93@gmail.com <harrisonarmstrong93@gmail.com>
2021-07-14 13:39:39 -0400

    Added more countries to the map

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 var config = {
     mapsApiKey: 'AIzaSyAicUJMDGxg1fEo1GRcNo3fGZhtf25rQGw',
-    NusicApiKey: 'a796737fb3bafc9bde347d07ad7780c1'
+    NusicApiKey: '585b3de09327313991ffd656d22ff7bb'
 }
